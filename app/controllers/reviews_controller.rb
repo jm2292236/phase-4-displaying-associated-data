60db41343c8f5ea356bd5b3e66fbb9faefae8289
Jaime Montes de Oca <jaime0422@live.com>
2022-08-27 22:22:48 -0700

    Completed Assignment

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,8 +1,17 @@
 class ReviewsController < ApplicationController
 
-  def index
-    reviews = Review.all.order(rating: :desc)
-    render json: reviews
-  end
+    def index
+        reviews = Review.all.order(rating: :desc)
+        render json: reviews, include: :dog_house
+    end
+
+    def show
+        review = Review.find_by(id: params[:id])
+        if review
+            render json: review, include: :dog_house
+        else
+            render json: {error: "Review not found"}, status: :not_found
+        end
+    end
 
 end
